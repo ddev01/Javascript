7b905abf1cffdd40b5afbd4021264b0f4e0030e0
douwe <d>
2021-02-18 13:46:07 +0100

    javascript

---
+++ H2/main.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "monke";
+document.getElementById("subkop").innerText = "monke 2";
+document.getElementById("monke").src = "img/monke.png";
+document.getElementById("monke2").src = "img/monke2.jpg";
